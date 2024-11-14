21e87f9c97e44d003cd4b0c89f5b404ac760debf
MirceaDan99 <mircea-aurelian.dan@intel.com>
2024-11-14 14:05:13 +0200

    Add `ov::internal::caching_with_mmap` property logic

--- src/plugins/intel_npu/src/plugin/src/plugin.cpp
+++ src/plugins/intel_npu/src/plugin/src/plugin.cpp
@@ -775,16 +775,27 @@ std::shared_ptr<ov::ICompiledModel> Plugin::import_model(std::istream& stream, c
         CompilerAdapterFactory compilerAdapterFactory;
         auto compiler = compilerAdapterFactory.getCompiler(_backends->getIEngineBackend(), localConfig);
 
-        auto graphSize = getFileSize(stream);
-
-        std::vector<uint8_t> blob(graphSize);
-        stream.read(reinterpret_cast<char*>(blob.data()), graphSize);
-        if (!stream) {
-            OPENVINO_THROW("Failed to read data from stream!");
+        std::shared_ptr<IGraph> graph;
+        if (compiler->getCompilerType() == ov::intel_npu::CompilerType::DRIVER) {
+            if (auto mmap_buffer = dynamic_cast<ov::OwningSharedStreamBuffer*>(stream.rdbuf())) {
+                graph = compiler->parse(mmap_buffer->get_buffer(), localConfig);
+                goto GRAPH_PARSED;
+            }
         }
-        _logger.debug("Successfully read %zu bytes into blob.", graphSize);
 
-        auto graph = compiler->parse(std::move(blob), localConfig);
+        {
+            auto graphSize = getFileSize(stream);
+
+            std::vector<uint8_t> blob(graphSize);
+            stream.read(reinterpret_cast<char*>(blob.data()), graphSize);
+            if (!stream) {
+                OPENVINO_THROW("Failed to read data from stream!");
+            }
+            _logger.debug("Successfully read %zu bytes into blob.", graphSize);
+
+            graph = compiler->parse(std::move(blob), localConfig);
+        }
+GRAPH_PARSED:
         graph->update_network_name("net" + std::to_string(_compiledModelLoadCounter++));
 
         const std::shared_ptr<ov::Model> modelDummy =
